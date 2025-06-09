7eea395537d2a03398fb864a8a8db80c89d07709
Sunny <singhsunny3009@gmail.com>
2025-06-09 12:34:49 +0530

    favicon added

--- FE/pages/_app.jsx
+++ FE/pages/_app.jsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css';
-
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo (1).svg" type="image/svg+xml" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
